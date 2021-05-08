dc604bff05a4745c2b268ccb26b4e175205e97bf
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-05-08 22:00:18 +0530

    action for phonebook

---
+++ src/actions/contactAction.js
@@ -0,0 +1,17 @@
+import {CREATE_CONTACT, GET_CONTACT, UPDATE_CONTACT} from '../constants/types';
+export const addContact = (contact)=>{
+    return {
+        type: CREATE_CONTACT,
+        payload : contact
+    }
+} 
+
+export const getContact = (id) => ({
+    type: GET_CONTACT,
+    payload : id 
+})
+
+export const updateContact = (contact) => ({
+    type : UPDATE_CONTACT,
+    payload : contact
+})
