f0a3e66b2bffa9dc61c8580b93178b5660f19542
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-05-12 13:30:39 +0530

    adding actions for select and unselect and deleteselected also

--- src/actions/contactAction.js
+++ src/actions/contactAction.js
@@ -1,4 +1,4 @@
-import {CREATE_CONTACT, GET_CONTACT,  UPDATE_CONTACT} from '../constants/types';
+import {CREATE_CONTACT, GET_CONTACT, DELETE_SELECTED,  UPDATE_CONTACT,SELECT_CONTACT, CLEAR_CONTACT} from '../constants/types';
 export const addContact = (contact)=>{
     return {
         type: CREATE_CONTACT,
@@ -19,4 +19,17 @@ export const updateContact = (contact) => ({
 export const deleteContact = (id) =>  ({
     type : "DELETE_CONTACT",
     payload : id 
+})
+
+export const selectAllContact = (id) => ({
+    type : SELECT_CONTACT,
+    payload : id
+})
+
+export const clearAllContact = () => ({
+    type : CLEAR_CONTACT
+})
+
+export const deleteSelectedContact = ()=> ({
+    type : DELETE_SELECTED
 })
